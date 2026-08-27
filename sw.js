fba4618f0a2496e110b742aa5bf50c116964ace3
yoonkukku <g8jgysnvty@privaterelay.appleid.com>
2026-08-28 05:48:57 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C="dapogi05";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon.png"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C="v06";self.addEventListener("fetch",e=>{});
